16cebfa31f917241384de5f2e82d48fe7277bc72
Craig Donavin <crdonavin@gmail.com>
2019-11-10 01:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "")
+  let right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
